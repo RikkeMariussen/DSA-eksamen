fd15668b70b8453fd4a8495533b0c003bb3f1023
RikkeMariussen <157796029+RikkeMariussen@users.noreply.github.com>
2025-12-04 10:32:20 +0100

    Init commit - not done yet

---
+++ script.js
@@ -0,0 +1,60 @@
+import { nodes, edges, graph, drawGraph, highlightNode, highlightEdge, sleep } from "./visualization.js"
+
+//The function for the algorithm will first run when the button is pressed, and our start node is "A"
+document.getElementById("runBtn").addEventListener("click", ()=>{dijkstra("A")});
+
+function dijkstra(start){
+    //First we set the variables, distance (dist), previous node(prev) and the array for shortest route (shortRoute). The shortRoute array is to keep track of the order of nodes that are shortest 
+    const dist = {};
+    const prev = {};
+    const shortRoute = [];
+
+    //As we are in the beginning of the algorithm, we do not know the distances or what node is after which, for therefor each node's distance is infinite and the prev is null
+    nodes.forEach(n => {
+        dist[n.id] = Infinity;
+        previousNode[n.id] = null;
+    });
+
+    //We do know that the starting distance is 0 - because we have not moved from the starting point
+    dist[start] = 0;
+    //We add the starting point to the shortRoute
+    shortRoute.push({id: start, dist: 0});
+
+    //Now we need to check each node and its belonging edges, to check if it is shorter than its alternative routes
+    //As long as the length (amount of objects) in shortRoute is more than 0 we stay in the while loop
+    while(shortRoute.length>0) {
+        shortRoute.sort((a, b) => a.dist - b.dist);
+        //Shift removes the first item in the array, but in order to look at the item, we store it in u
+        const u = shortRoute.shift();
+        //Because we just looked/is looking at u, we want to highlight it
+        highlightNode(u.id);
+        //And now we need to wait abit, so our human eyes can register what is happening
+        await sleep(700);
+
+        //Now we need to check each of the neighbors at the current node we are looking at, u
+        for(const neighbor of graph[u.id]) {
+            //As our next move maybe will be to that neighboring node, we need to know the distance 
+            const newDist = dist[u.id] + neighbor.weight;
+            //Now in case that the distance to the neighboring node we are looking at is SMALLER that what is currently sat at the neighboring node's distance we want to the change primary neighbor
+            if (newDist < dist[neighbor.node]) {
+                dist[neighbor.node] = newDist;
+                //The previous node for the node we will look at next, is the node we are currently looking at
+                prev[neighbor.node] = u.id;
+                //As this route is found to be shorter than already was put in as the u.ids neighbor, we need to push this neighboring node to our route
+                shortRoute.push({id: neighbor.node, dist: newDist});
+                //And now we need to find the edges that we want to highlight, which is from the u.id to the neighboring node
+                const edge = edges.find(e =>
+                    (e.from == u.id && e.to == neighbor.node) ||
+                    (e.to == u.id && e.from == neighbor.node)
+                );
+
+                highlightEdge(edge);
+                //As we want the users to be able to register the highlights, we wait 700 ms
+                await sleep(700);
+            }
+            //If the newDist is NOT smaller than the current dist+weight, we will just go to the next node in our for each loop
+        }
+    }
+
+    return {dist, prev};
+}
---
+++ visualization.js
@@ -0,0 +1,98 @@
+export const canvas = document.getElementById("graphCanvas");
+export const ctx = canvas.getContext("2d");
+
+//Setting the nodes value(id) and their places on the graph
+export const nodes = [
+    {id: "A", x: 100, y: 100},
+    {id: "B", x: 300, y: 80},
+    {id: "C", x: 500, y: 150},
+    {id: "D", x: 150, y: 300},
+    {id: "E", x: 400, y: 320}
+];
+
+//Setting the lines between the nodes, and which nodes and the weight between them
+export const edges = [
+    {from: "A", to: "B", weight: 4},
+    {from: "A", to: "D", weight: 2},
+    {from: "B", to: "C", weight: 5},
+    {from: "D", to: "E", weight: 7},
+    {from: "B", to: "E", weight: 10},
+    {from: "C", to: "E", weight: 3}
+];
+
+//Making the graph with the nodes and edges
+export const graph = {};
+
+nodes.forEach(n => {
+    graph[n.id] = [];
+});
+edges.forEach(e => {
+    graph[e.from].push({node: e.to, weight: e.weight});
+    graph[e.to].push({node: e.from, weight: e.weight});
+});
+
+
+export function drawGraph(highlight = {}) {
+    //Drawing a rectangle that starts at 0,0 on x, y and has the width and height given in index.html
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    //Finding where the edges in the graph is and drawing it, for each of edges
+    edges.forEach(e => {
+        //Find the node with the id that is equal to either the from or to e.id
+        const a = nodes.find(n => n.id == e.from);
+        const b = nodes.find(n => n.id == e.to); 
+
+        //Drawing the edges, first be beginPath()
+        ctx.beginPath();
+        //Setting the stroke style, as we wish to highlight the edge we are currently on in our visualization
+        ctx.strokeStyle = highlight.edge == e ? "red" : "black";
+        //Making the line between the from and the to points. (otherwise there is just to dots coloured, which will be covered up later by nodes)
+        ctx.moveTo(a.x, a.y);
+        ctx.lineTo(b.x, b.y);
+        //Stroke draws in what we just coded
+        ctx.stroke();
+
+        //Now we draw the weight from "from" to "to", in the middle of the line
+        const middleX = (a.x + b.x)/2;
+        const middleY = (a.y + b.y)/2;
+        ctx.fillText(e.weight, middleX, middleY);
+    });
+
+    //We drew the edges first, so the lines do not cross OVER our drawing of nodes
+    nodes.forEach(n => {
+        ctx.beginPath();
+        //Setting the fillstyle, as we wish to highlight the node we are currently on in our visualization
+        ctx.fillStyle = highlight.node == n ? "yellow" : "lightyellow";
+        ctx.strokeStyle = "black";
+        //Making the shape a circle to indicate that this is the node
+        //arc needs: x, y, radius, start angle and end angle
+        ctx.arc(n.x, n.y, 20, 0, Math.PI*2);
+        //Fill colours the cirle, and stroke draws in what we just coded, on top of what else is drawn
+        ctx.fill();
+        ctx.stroke();
+
+        //We want the node's id on top of the circle, written in black
+        ctx.fillStyle = "black";
+        //Making sure the text is not on top of the line, but in the middle
+        ctx.fillText(n.id, n.x - 5, n.y + 5);
+    });
+}
+
+//Call the function - which is shown as we call the script in the index.html
+drawGraph();
+
+//Calling the graph to highlight the appropriate node
+export function highlightNode(id) {
+    drawGraph({node: id});
+}
+
+
+//Calling the graph to highlight the appropriate edge
+export function highlightEdge(edge) {
+    drawGraph({edge});
+}
+
+//We also need to make sure that this visualization is not over before our eyes can register it, so we need a sleep function
+export function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+} 
