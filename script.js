66365617d257fe9d469f258e17779dfad244139f
RikkeMariussen <157796029+RikkeMariussen@users.noreply.github.com>
2025-12-04 11:07:58 +0100

    comments went missing because of merging issues

--- script.js
+++ script.js
@@ -33,21 +33,29 @@ async function dijkstra(start){
         //And now we need to wait a bit, so our human eyes can register what is happening
         await sleep(700);
 
+        //Now we need to check each of the neighbors at the current node we are looking at, u
         for(const neighbor of graph[u.id]) {
+            //As our next move maybe will be to that neighboring node, we need to know the distance 
             const newDist = dist[u.id] + neighbor.weight;
+            //Now in case that the distance to the neighboring node we are looking at is SMALLER that what is currently sat at the neighboring node's distance we want to the change primary neighbor
             if (newDist < dist[neighbor.node]) {
                 dist[neighbor.node] = newDist;
+                //The previous node for the node we will look at next, is the node we are currently looking at
                 prev[neighbor.node] = u.id;
+                //As this route is found to be shorter than already was put in as the u.ids neighbor, we need to push this neighboring node to our route
                 shortRoute.push({ id: neighbor.node, dist: newDist });
 
+                //And now we need to find the edges that we want to highlight, which is from the u.id to the neighboring node
                 const edge = edges.find(e =>
                     (e.from == u.id && e.to == neighbor.node) ||
                     (e.to == u.id && e.from == neighbor.node)
                 );
 
                 highlightEdge(edge);
+                //As we want the users to be able to register the highlights, we wait 700 ms
                 await sleep(700);
             }
+        //If the newDist is NOT smaller than the current dist+weight, we will just go to the next node in our for each loop
         }
     }
 
